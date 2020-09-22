f962e8518abfbb5aa687ba09a208c9ea5b931f36
Jiawei Fu <jiawfu@student.ethz.ch>
2020-09-23 00:04:26 +0200

    jointToQuat

--- robotdynamics_exercise_1a/problems/jointToQuat.m
+++ robotdynamics_exercise_1a/problems/jointToQuat.m
@@ -5,4 +5,11 @@ function quat = jointToQuat(q)
   
   % PLACEHOLDER FOR OUTPUT -> REPLACE WITH SOLUTION
   quat = zeros(4,1);
+  % First calculate rotation matrix
+  C = jointToRotMat(q);
+  quat(1) = sqrt(C(1,1)+C(2,2)+C(3,3)+1);
+  quat(2) = sign(C(3,2)-C(2,3))*sqrt(C(1,1)-C(2,2)-C(3,3)+1);
+  quat(3) = sign(C(1,3)-C(3,1))*sqrt(C(2,2)-C(3,3)-C(1,1)+1);
+  quat(4) = sign(C(2,1)-C(1,2))*sqrt(C(3,3)-C(1,1)-C(2,2)+1);
+  quat = 0.5*quat;
 end
