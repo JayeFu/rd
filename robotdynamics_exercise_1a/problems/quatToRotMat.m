9cb624f08b80de5e9fed46e97f000cc02e403186
Jiawei Fu <jiawfu@student.ethz.ch>
2020-09-23 00:23:34 +0200

    quatToRotMat

--- robotdynamics_exercise_1a/problems/quatToRotMat.m
+++ robotdynamics_exercise_1a/problems/quatToRotMat.m
@@ -4,4 +4,11 @@ function R = quatToRotMat(q)
   
   % PLACEHOLDER FOR OUTPUT -> REPLACE WITH SOLUTION
   R = zeros(3);
+  % qh = [x y z]
+  qh = q(2:4);
+  % anti-symmetric matrix of qh
+  qas = [0, -qh(3), qh(2);
+      qh(3), 0, -qh(1);
+      -qh(2), qh(1), 0];
+  R = (2*q(1)^2-1)*eye(3)+2*q(1)*qas+2*qh*qh';
 end
