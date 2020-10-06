2ebb5853857813eb604e1d563391a315e09e252f
Jiawei Fu <jiawfu@student.ethz.ch>
2020-10-06 21:39:39 +0200

    pinv finished

--- robotdynamics_exercise_1c/problems/pseudoInverseMat.m
+++ robotdynamics_exercise_1c/problems/pseudoInverseMat.m
@@ -8,4 +8,13 @@ function [ pinvA ] = pseudoInverseMat(A, lambda)
 % TODO: complete the computation of the pseudo-inverse.
 % Hint: How should we account for both left and right pseudo-inverse forms?
 pinvA = zeros(n, m);
+
+% Whether left inverse or right inverse
+if m>=n % left inverse
+    pinvA = (A'*A+lambda^2*eye(n))\A';
+elseif m<=n
+    pinvA = A'/(A*A'+lambda^2*eye(m));
+else
+    disp('Sth goes wrong')
+end
 end
